a7e4badb828d149c8e37f4c83ed61b01afec5204
kxnvg <nikita.armeec.88@mail.ru>
2023-12-06 15:32:11 +0300

    Create new project kameleoon-quotes

---
+++ src/test/java/com/kxnvg/kameleoon/LifeQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kxnvg.kameleoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeQuotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
